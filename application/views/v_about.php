62a040ccd61dc0a6a43b73fd72b56b679f5be0f9
Ryan <ryanferdiansyach@gmail.com>
2021-10-01 14:13:10 +0700

    tugas III template

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ryan</li>
+        <li>Nama Belakang : Ferdiansyach</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kp. Padurenan Cibinong, Bogor</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Basket</li>
+        <li>Renang</li>
+    </ul>
+</section>
